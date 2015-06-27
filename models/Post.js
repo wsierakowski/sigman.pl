787cd50df765cdfa0dfc40746fa5a5e1eb8656c7
wsierakowski <wsierakowski@gmail.com>
2015-06-27 13:50:54 +0100

    Added markup and jade as options to the content in the Post model.

--- models/Post.js
+++ models/Post.js
@@ -18,8 +18,11 @@ Post.add({
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.CloudinaryImage },
 	content: {
-		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-		extended: { type: Types.Html, wysiwyg: true, height: 400 }
+		brief: { type: Types.Html, wysiwyg: true, height: 150, required: true, default: 'Enter brief here as text.' },
+		extendedType: { type: Types.Select, options: 'html, markdown, jade', default: 'html' },
+		extendedHTML: { type: Types.Html, wysiwyg: true, height: 200, dependsOn: {'content.extendedType': 'html'} },
+		extendedMarkdown: { type: Types.Markdown, wysiwyg: true, height: 200, dependsOn: {'content.extendedType': 'markdown'} },
+		extendedJade: { type: Types.Code, language: 'jade', wysiwyg: true, height: 200, dependsOn: {'content.extendedType': 'jade'} }
 	},
 	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
 	tags: { type: Types.Relationship, ref: 'PostTag', many: true}
@@ -27,6 +30,18 @@ Post.add({
 
 Post.schema.virtual('content.full').get(function() {
 	return this.content.extended || this.content.brief;
+});
+
+Post.schema.virtual('content.isExtended').get(function() {
+	return this.content.extendedType ===  'html' && !!this.content.extendedHTML ||
+					this.content.extendedType ===  'markdown' && !!(this.content.extendedMarkdown && this.content.extendedMarkdown.md) ||
+					this.content.extendedType ===  'jade' && !!this.content.extendedJade;
+});
+
+Post.schema.virtual('content.getExtended').get(function() {
+	return this.content.extendedType ===  'html' && this.content.extendedHTML ||
+					this.content.extendedType ===  'markdown' && (this.content.extendedMarkdown && this.content.extendedMarkdown.html) ||
+					this.content.extendedType ===  'jade' && this.content.extendedJade;
 });
 
 Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
